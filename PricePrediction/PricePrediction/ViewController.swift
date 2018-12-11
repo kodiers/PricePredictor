8a195f549e8f9e05389acf6145b4733ca067a725
Victor Yamchinov <kodiers@gmail.com>
2018-12-11 03:18:08 +0300

    created project

---
+++ PricePrediction/PricePrediction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PricePrediction
+//
+//  Created by Viktor Yamchinov on 11/12/2018.
+//  Copyright © 2018 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
