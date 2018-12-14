552fa574f56d53cd5b4c1a4ba1f05d7739fe5e4e
Victor Yamchinov <kodiers@gmail.com>
2018-12-14 03:59:25 +0300

    created ui

--- PricePrediction/PricePrediction/ViewController.swift
+++ PricePrediction/PricePrediction/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var numberOfRooms: UISegmentedControl!
+    @IBOutlet weak var numberOfBathrooms: UISegmentedControl!
+    @IBOutlet weak var garageCapacity: UISegmentedControl!
+    @IBOutlet weak var condition: UISegmentedControl!
+    @IBOutlet weak var yearBuiltLbl: UILabel!
+    @IBOutlet weak var yearBuiltSlider: UISlider!
+    @IBOutlet weak var sizeLbl: UILabel!
+    @IBOutlet weak var sizeSlider: UISlider!
+    @IBOutlet weak var resultLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func updatePrediction(_ sender: Any) {
+    }
+    
 }
 
