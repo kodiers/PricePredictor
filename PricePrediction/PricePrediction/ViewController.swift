7bd94271f7af55c402d9fddfa32d4a3caed5c9e2
Victor Yamchinov <kodiers@gmail.com>
2018-12-18 05:09:45 +0300

    added prediction handler

--- PricePrediction/PricePrediction/ViewController.swift
+++ PricePrediction/PricePrediction/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let model = HousePrices()
 
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var numberOfRooms: UISegmentedControl!
@@ -24,9 +26,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let spacing: CGFloat = 30
+        stackView.setCustomSpacing(spacing, after: numberOfRooms)
+        stackView.setCustomSpacing(spacing, after: numberOfBathrooms)
+        stackView.setCustomSpacing(spacing, after: garageCapacity)
+        stackView.setCustomSpacing(spacing, after: yearBuiltSlider)
+        stackView.setCustomSpacing(spacing, after: sizeSlider)
+        stackView.setCustomSpacing(spacing, after: condition)
+        updatePrediction(self)
+        
     }
 
     @IBAction func updatePrediction(_ sender: Any) {
+        yearBuiltLbl.text = "Year Built: \(Int(yearBuiltSlider.value))"
+        sizeLbl.text = "Size: \(Int(sizeSlider.value))"
+        do {
+            let prediction = try model.prediction(
+                bathrooms: Double(numberOfBathrooms.selectedSegmentIndex + 1),
+                cars: Double(garageCapacity.selectedSegmentIndex),
+                condition: Double(condition.selectedSegmentIndex),
+                rooms: Double(numberOfRooms.selectedSegmentIndex + 1),
+                size: Double(Int(sizeSlider.value)),
+                yearBuilt: Double(Int(yearBuiltSlider.value))
+            )
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            formatter.maximumFractionDigits = 0
+            resultLbl.text = formatter.string(from: prediction.value as NSNumber) ?? ""
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
 }
